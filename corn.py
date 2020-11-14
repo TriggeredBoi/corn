5a00eae099ba03714641ae340ab97bacf7a3825a
Triggeredboi <lucas.moreno2124@gmail.com>
2020-11-14 02:06:13 -0300

    corn reaction, popcorn, leaderboards

--- corn.py
+++ corn.py
@@ -1,10 +1,12 @@
 import os
 import re
 import json
+import random
 
 
 #below are the ones that aren't python builtins
-from discord.ext import commands
+import discord
+from discord.errors import Forbidden
 from dotenv import load_dotenv
 
 
@@ -21,7 +23,7 @@ if not token:
     print(f"The \"token\" field is empty - Please open \"{configpath}\" and put the discord bot token and Server ID in their respective fields.")
     exit()
 
-bot = commands.Bot(command_prefix="!")
+bot = discord.ext.commands.Bot(command_prefix="!")
 
 @bot.event
 async def on_ready():
@@ -34,12 +36,13 @@ class CornController():
         self.loadcorn()
 
     def addcorn(self, msg, corns):
-        who = msg.author.id
+        who = str(msg.author.id)
 
-        total = self.cornleaderboard.setdefault(who, 0)
+        total = self.cornleaderboard.get(who) or 0
         total += len(corns)
 
         self.cornleaderboard[who] = total
+        self.savecorn()
         return total
     
     def loadcorn(self):
@@ -59,16 +62,33 @@ Corn = CornController()
 
 @bot.event
 async def on_message(msg):
-    print(msg.content.lower())
-    corns = re.findall("corn|🌽", msg.content.lower())
+    if msg.author == bot: return
+    corns = re.findall("corn|🌽|🍿", msg.content.lower())
     if not corns:
         return
     total = Corn.addcorn(msg, corns)
     print(f"{msg.author} corned {len(corns)} times, now at a total of {total}.")
+    try: await msg.add_reaction("🌽")
+    except discord.errors.Forbidden: pass
+    if random.random() == 100:
+        try: await msg.channel.send("corn")
+        except discord.errors.Forbidden: pass
+    print(msg.channel)
+    await bot.process_commands(msg)
 
 @bot.command()
-async def save(ctx):
-    Corn.savecorn()
+async def corn(ctx):
+    txt = ""
+
+    sortedboard = sorted(Corn.cornleaderboard.items(), key=lambda item: item[1], reverse=True) #(list of tuples)
+    items = iter(sortedboard) #ghetto generator? or is a generator an advanced iterator? either way, this works.
+
+    for i in range( min(len(sortedboard), 10) ):
+        id, amt = next(items)
+        txt += f"#{i+1} | <@{id}> - {amt}\n"
+        
+    embed=discord.Embed(title="🌽 Leaderboards", description=txt)
+    await ctx.send(embed=embed)
 
 
 if __name__ == "__main__":
